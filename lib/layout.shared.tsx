a5659992e00a6ad09aa63935329ddb878a3c0e85
tshewang <jigmedradrel@gmail.com>
2026-03-19 10:56:13 +0600

    fix: update repository name in layout config

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: "tshewangrinzin",
-  repo: "technology-notes",
+  repo: "tech-notes",
   branch: "main",
 };
 
