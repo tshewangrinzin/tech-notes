8d968d41f6c08bc7691a87f386abbe4581ef1cd0
tshewang <jigmedradrel@gmail.com>
2026-03-23 17:39:03 +0600

    added tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
+import type { Metadata } from "next";
 import "./global.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/logo.webp",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
